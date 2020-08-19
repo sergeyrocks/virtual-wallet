7322db71f6d2361b5369781ff04475e6f60c2c79
Sergey <sergeyrockit@gmail.com>
2020-08-19 19:30:06 +0300

    Auth scaffold, removed unused files, cleanup, packages

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +20,8 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => Hash::make('1'),
         'remember_token' => Str::random(10),
     ];
 });

7dd8a42e9ea565fa270e494c9c945b8d72d44bea
Sergey <sergeyrockit@gmail.com>
2021-03-24 20:13:52 +0200

    Improved validation, controllers markup clean

---
+++ app/Rules/PositiveNumber.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class PositiveNumber implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return $value > 0;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must be greater than 0.';
+    }
+}

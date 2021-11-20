2b283a2ca1e244b7154d448b7fd27bae3d5b8863
Sergey <sergeyrockit@gmail.com>
2021-11-20 16:16:55 +0200

    Transaction test code style improve

--- tests/Feature/TransactionStoreTest.php
+++ tests/Feature/TransactionStoreTest.php
@@ -3,35 +3,42 @@
 use App\Models\Transaction;
 use App\Models\User;
 use App\Models\Wallet;
+
 use function Pest\Faker\faker;
 
 it('stores transaction', function () {
     /** @var User $user */
     $user = User::factory()
-        ->has(Wallet::factory())
         ->create();
 
-    $wallet = $user->wallets->first();
+    /** @var Wallet $wallet */
+    $wallet = Wallet::factory()
+        ->for($user)
+        ->create();
 
     $balanceBeforeTransaction = $wallet->balance;
 
     /** @var Transaction $transaction */
-    $transaction = Transaction::factory()->for($wallet)->create();
+    $transaction = Transaction::factory()
+        ->for($wallet)
+        ->state(function (array $attributes) use ($user) {
+            $attributes['payer'] = $attributes['is_incoming']
+                ? faker()->name . ' ' . faker()->iban
+                : $user->email;
 
-    $transaction->payer = $transaction->is_incoming ?
-        faker()->name . ' ' . faker()->iban() :
-        $user->email;
+            $attributes['beneficiary'] = $attributes['is_incoming']
+                ? $user->email
+                : faker()->name . ' ' . faker()->iban;
 
-    $transaction->beneficiary = !$transaction->is_incoming ?
-        faker()->name . ' ' . faker()->iban() :
-        $user->email;
-
-    $wallet->balance = $transaction->is_incoming ?
-        bcadd($wallet->balance, $transaction->amount) :
-        bcsub($wallet->balance, $transaction->amount);
-
-    $transaction->save();
-    $wallet->save();
+            return $attributes;
+        })
+        ->afterCreating(function (Transaction $transaction) use ($wallet) {
+            $wallet->balance = $transaction->is_incoming
+                ? bcadd($wallet->balance, $transaction->amount)
+                : bcsub($wallet->balance, $transaction->amount);
+            $wallet->save();
+        })
+        ->create();
 
     if ($transaction->is_incoming) {
         $this->assertTrue($wallet->balance > $balanceBeforeTransaction);
